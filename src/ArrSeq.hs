5b48794c9f5ad49c4057df87ac82e0836ea357eb
alanhergen <alan.hergen@gmail.com>
2026-06-08 21:28:48 -0300

    implement all functions in ArrSeq except reduce and scan

--- src/ArrSeq.hs
+++ src/ArrSeq.hs
@@ -1,56 +1,100 @@
 module ArrSeq where
 
 import qualified Arr as A
---import Arr (!)
+import Seq
+import Par
+import Arr ((!))
+import Seq (TreeView(EMPTY))
 
-instance Seq A where
-    emptyS     :: s a
-    emptyS = empty 
+instance Seq A.Arr where
+    emptyS :: A.Arr a
+    emptyS = A.empty 
 
-    singletonS :: a -> s a
-    singletonS x = fromList [x] 
+    singletonS :: a -> A.Arr a
+    singletonS x = A.fromList [x] 
 
-    lengthS    :: s a -> Int 
-    lengthS = length 
+    lengthS :: A.Arr a -> Int 
+    lengthS = A.length 
 
-    nthS       :: s a -> Int -> a 
-    nthS s n = s A.! n 
+    nthS :: A.Arr a -> Int -> a 
+    nthS s n = s ! n
 
-    tabulateS  :: (Int -> a) -> Int -> s a
-    tabulateS = tabulate
+    tabulateS :: (Int -> a) -> Int -> A.Arr a
+    tabulateS = A.tabulate
 
-    mapS       :: (a -> b) -> s a -> s b 
+    mapS :: (a -> b) -> A.Arr a -> A.Arr b 
     mapS f s
         | lengthS s == 0 = emptyS
-        | lengthS s == 1 = Singleton (f (nthS s 1))
+        | lengthS s == 1 = singletonS (f (nthS s 0))
         | otherwise = 
             let
-                (l, r) = subArray 0 (div (lengthS s) 2) s ||| subArray (div (lengthS s) 2 + 1) (lengthS s) s 
-                (ml, mr) = map f l ||| map f r
-            in flatten (fromList [ml, mr])
+                (l, r) = A.subArray 0 (div (lengthS s) 2) s ||| A.subArray (div (lengthS s) 2 + 1) (lengthS s) s 
+                (ml, mr) = mapS f l ||| mapS f r
+            in A.flatten (A.fromList [ml, mr])
 
-    filterS    :: (a -> Bool) -> s a -> s a 
+    filterS :: (a -> Bool) -> A.Arr a -> A.Arr a 
     filterS f s 
         | lengthS s == 0 = emptyS
-        | lengthS s == 1 = if f (nthS s 1) then Singleton (f (nthS s 1)) else emptyS 
+        | lengthS s == 1 = if f (nthS s 0) then singletonS (nthS s 0) else emptyS 
         | otherwise = 
             let
-                (l, r) = subArray 0 (div (lengthS s) 2) s ||| subArray (div (lengthS s) 2 + 1) (lengthS s) s 
+                (l, r) = A.subArray 0 (div (lengthS s) 2) s ||| A.subArray (div (lengthS s) 2 + 1) (lengthS s) s 
                 (ml, mr) = filterS f l ||| filterS f r
-            in flatten (fromList [ml, mr])
+            in A.flatten (A.fromList [ml, mr])
 
-    appendS    :: s a -> s a -> s a
-    appendS s r = flatten fromList [s,r]
+    appendS :: A.Arr a -> A.Arr a -> A.Arr a
+    appendS s r = A.flatten (A.fromList [s,r])
 
-    takeS      :: s a -> Int -> s a
-    takeS s n = subArray 0 (min n (lengthS s)) s
+    takeS :: A.Arr a -> Int -> A.Arr a
+    takeS s n = A.subArray 0 (min n (lengthS s)) s
 
-    dropS      :: s a -> Int -> s a
-    dropS s n = subArray (max (length s - n) 0) n s
+    dropS :: A.Arr a -> Int -> A.Arr a
+    dropS s n = A.subArray n (lengthS s - n)  s
 
-    -- showtS     :: s a -> TreeView a (s a)
-    -- showlS     :: s a -> ListView a (s a)
-    -- joinS      :: s (s a) -> s a
-    -- reduceS    :: (a -> a -> a) -> a -> s a -> a
-    -- scanS      :: (a -> a -> a) -> a -> s a -> (s a, a)
-    -- fromList   :: [a] -> s a
+    showtS :: A.Arr a -> TreeView a (A.Arr a)
+    showtS arr
+        | lengthS arr == 0 = EMPTY
+        | lengthS arr == 1 = ELT (nthS arr 0)
+        | otherwise       = NODE (takeS arr m) (dropS arr m)
+            where m = div (lengthS arr) 2
+    
+    showlS :: A.Arr a -> ListView a (A.Arr a) 
+    showlS arr
+        | lengthS arr == 0 = NIL
+        | otherwise        = CONS (nthS arr 0) (dropS arr 1)
+
+    joinS :: A.Arr (A.Arr a) -> A.Arr a
+    joinS = A.flatten
+
+    -- reduceS :: (a -> a -> a) -> a -> A.Arr a -> a
+    -- scanS :: (a -> a -> a) -> a -> A.Arr a -> (A.Arr a, a)
+    
+    fromList :: [a] -> A.Arr a
+    fromList = A.fromList
+
+{- ===== AUX ===== -}
+contr :: (a -> a -> a) -> A.Arr a -> A.Arr a
+contr f arr 
+    | A.length arr == 0 = A.empty
+    | A.length arr == 1 = singletonS (nthS arr 0)
+    | A.length arr == 2 = singletonS (f (nthS arr 0) (nthS arr 1))
+    | otherwise        = 
+        let
+            idx = 2 ^ ilg (A.length arr - 1)
+            (contraction, list) = contr f (takeS arr 2) ||| contr f (dropS arr 2)
+        in  appendS contraction list
+
+-- contr :: (a -> a -> a) -> A.Arr a -> A.Arr a
+-- contr f arr 
+--     | A.length arr == 0 = A.empty
+--     | A.length arr == 1 = singletonS (nthS arr 0)
+--     | A.length arr == 2 = singletonS (f (nthS arr 0) (nthS arr 1))
+--     | otherwise        = 
+--         let
+--             idx = 2 ^ ilg (A.length arr - 1)
+--             (l, r) = contr f (takeS arr idx) ||| contr f (dropS arr idx)
+--         in  appendS l r
+
+-- Costo: O(1)
+ilg :: Int -> Int
+ilg x = floor (logBase 2 (fromIntegral x))
